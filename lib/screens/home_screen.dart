785137731edd6ed5a0efd0c1385719057c761872
faresarar <faresarar455@gmail.com>
2024-08-04 17:01:45 +0100

    create search_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/no_weather_body.dart';
+import 'search_screen.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Weather App"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const SearchScreen();
+                  },
+                ),
+              );
+            },
+            icon: const Icon(Icons.search),
+          )
+        ],
+      ),
+      body: const NoWeatherBody(),
+    );
   }
 }
