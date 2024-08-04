5eb79d8f73f84f48578c50838c10f8f033dd4f82
faresarar <faresarar455@gmail.com>
2024-08-04 17:23:11 +0100

    adding TextField

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -5,9 +5,40 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text("Search City "),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Center(
+          child: SizedBox(
+            height: 200,
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 32,
+                  horizontal: 16,
+                ),
+                hintText: "Entre city name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: const BorderSide(
+                    color: Colors.green,
+                  ),
+                ),
+                hintStyle: const TextStyle(),
+                suffixIcon: const Icon(Icons.search),
+                labelText: "Search",
+
+                ///icon: const Icon(Icons.telegram) /// to put icon before TextField
+                ///enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.green),),
+                ///focusedBorder: const OutlineInputBorder(),
+                ///enabled: false ,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
