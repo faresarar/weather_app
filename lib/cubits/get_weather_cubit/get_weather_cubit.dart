5fff544a9ac10bb3d597b571fa556a35a9ee0938
faresarar <faresarar455@gmail.com>
2024-08-05 18:21:52 +0100

    Creating Cubit

--- lib/cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -1,3 +1,7 @@
-class GetWeatherCubit {
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'get_weather_states.dart';
+
+class GetWeatherCubit extends Cubit<WeatherState> {
+  GetWeatherCubit(super.initialState);
 }
