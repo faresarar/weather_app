fba8b8b6d41d51e7aa462275861c2ee9525a9f87
faresarar <faresarar455@gmail.com>
2024-08-05 21:22:19 +0100

    Change background color

--- lib/widgets/weather_info_body.dart
+++ lib/widgets/weather_info_body.dart
@@ -12,67 +12,120 @@ class WeatherInfoBody extends StatelessWidget {
     WeatherModel weatherModel =
         BlocProvider.of<GetWeatherCubit>(context).weatherModel!;
     weatherModel.image!.contains("https");
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomText(weatherModel: weatherModel),
-          Text(
-            'updated at ${weatherModel.date.hour}:${weatherModel.date.minute}',
-            style: const TextStyle(
-              fontSize: 24,
-            ),
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.network(
-                "https:${weatherModel.image}",
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            getThemeColor(weatherModel.weatherCondition),
+            getThemeColor(weatherModel.weatherCondition)[300]!,
+            getThemeColor(weatherModel.weatherCondition)[50]!,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomText(weatherModel: weatherModel),
+            Text(
+              'updated at ${weatherModel.date.hour}:${weatherModel.date.minute}',
+              style: const TextStyle(
+                fontSize: 24,
               ),
-              Text(
-                weatherModel.temp.toString(),
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 32,
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.network(
+                  "https:${weatherModel.image}",
                 ),
-              ),
-              Column(
-                children: [
-                  Text(
-                    "Maxtemp :${weatherModel.maxTemp.round()}",
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
+                Text(
+                  weatherModel.temp.toString(),
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
                   ),
-                  Text(
-                    "Mintemp :${weatherModel.minTemp.round()}",
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Maxtemp :${weatherModel.maxTemp.round()}",
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      "Mintemp :${weatherModel.minTemp.round()}",
 
-                    /// تقريب الى اقرب رقم صحيح
-                    style: const TextStyle(
-                      fontSize: 16,
+                      /// تقريب الى اقرب رقم صحيح
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          Text(
-            weatherModel.weatherCondition,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 32,
+                  ],
+                ),
+              ],
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 32,
+            ),
+            Text(
+              weatherModel.weatherCondition,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  MaterialColor getThemeColor(String? condition) {
+    if (condition == null) {
+      return Colors.blue;
+    }
+    switch (condition) {
+      case "Sunny":
+      case "clear":
+      case "Partly cloudy":
+      case "Cloudy":
+      case "Overcast":
+        return Colors.blueGrey;
+      case "Mist":
+      case "Fog":
+      case "Freezing fog":
+        return Colors.blueGrey;
+      case "Patchy rain possible":
+      case "Patchy light rain":
+      case "Light rain ":
+      case "Moderate rain at times":
+      case "Moderate rain":
+      case "Heavy rain at times":
+      case "Heavy rain":
+      case "Light rain shower":
+      case "Moderate or Heavy rain shower":
+      case "Torrential rain shower":
+        return Colors.lightBlue;
+      case "Patchy snow possible":
+      case "Patchy light snow":
+      case "Light snow":
+      case "Patchy moderate snow":
+      case "Moderate snow":
+      case "Heavy snow":
+      case "Light snow showers":
+      case "Moderate or heavy snow showers":
+        return Colors.lightBlue;
+      default:
+        return Colors.grey;
+    }
   }
 }
 
